1be2a9e4441fb2e9e82a4256cb4cca320159d1e7
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-05-27 11:44:59 +0000

    + First attempt at a rendering step.

--- apps/VideoPlayer/src/ibis/videoplayer/RenderFrameJob.java
+++ apps/VideoPlayer/src/ibis/videoplayer/RenderFrameJob.java
@@ -1,9 +1,13 @@
 package ibis.videoplayer;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
 
 import ibis.maestro.Context;
 import ibis.maestro.Node;
+import ibis.util.RunProcess;
 
 /**
  * An action to run a PovRay scene description through povray, and load the
@@ -16,58 +20,96 @@ public class RenderFrameJob implements ibis.maestro.Job
 {
     private static final long serialVersionUID = -3938044583266505212L;
 
-    private final String iniFile;
-    
+    private final String iniFileContent;
+
     /**
-     * @param iniFile
+     * @param iniFileContent
      */
-    RenderFrameJob( String iniFile )
+    RenderFrameJob( String iniFileContent )
     {
-	this.iniFile = iniFile;
+        this.iniFileContent = iniFileContent;
     }
 
     class RenderInfo implements Serializable
     {
-	final int width;
-	final int height;
-	final int frame;
-	final String scene;
+        private static final long serialVersionUID = 1899219003828691971L;
+        final int width;
+        final int height;
+        final int frameno;
+        final String scene;
 
-	/**
-	 * @param width
-	 * @param height
-	 * @param frame
-	 * @param scene
-	 */
-	RenderInfo(int width, int height, int frame, String scene) {
-	    this.width = width;
-	    this.height = height;
-	    this.frame = frame;
-	    this.scene = scene;
-	}
+        /**
+         * @param width
+         * @param height
+         * @param frame
+         * @param scene
+         */
+        RenderInfo(int width, int height, int frame, String scene) {
+            this.width = width;
+            this.height = height;
+            this.frameno = frame;
+            this.scene = scene;
+        }
     }
-    
-    private static void writeFile( String filename, String content )
+
+    private static boolean writeFile( File f, String s ) throws IOException
     {
-
+        boolean ok = f.delete();  // First make sure it doesn't exist.
+        FileWriter output = new FileWriter( f );
+        output.write( s );
+        output.close();
+        return ok;
     }
 
-    /** Runs this job.
-     * @return The decompressed frame.
+    /** Runs this render job.
+     * @return The rendered frame.
      */
     @Override
     public Object run( Object obj, Node node, Context context )
     {
-	
-	RenderInfo info = (RenderInfo) obj;
-	
-	writeFile( String.format( "frame-%06d.pov", info.frame ), info.scene );
-	writeFile( String.format( "frame-%06d.ini", info.frame ), iniFile );
-	String command[] = {
-		"povray",
-		"+FP",
-		
-	};
-	return null;
+        RenderInfo info = (RenderInfo) obj;
+        final File povFile = new File( String.format( "frame-%06d.pov", info.frameno ) );
+        final File iniFile = new File( String.format( "frame-%06d.ini", info.frameno ) );
+        final File outFile = new File( String.format( "frame-%06d.ppm", info.frameno ) );
+        UncompressedImage img = null;
+
+        try {
+            writeFile( povFile, info.scene );
+            writeFile( iniFile, iniFileContent );
+        }
+        catch( IOException e ) {
+            System.err.println( "Cannot write render input file: " + e.getLocalizedMessage() );
+            return null;
+        }
+        String command[] = {
+                "povray",
+                iniFile.getAbsolutePath(),
+                "+I" + povFile.getAbsolutePath(),
+                "+O" + outFile.getAbsolutePath(),
+                "+H" + info.height,
+                "+W" + info.width,
+                "+FP",
+                "+Q9",
+                "+A0.3"
+        };
+        try {
+            RunProcess p = new RunProcess( command );
+            int exit = p.getExitStatus();
+            if( exit != 0 ) {
+                System.err.println( "Rendering of '" + povFile + "' failed:" );
+                System.err.println( p.getStdout() );
+                System.err.println( p.getStderr() );
+                return null;
+            }
+            img = UncompressedImage.load( outFile, info.frameno );
+            povFile.delete();
+            iniFile.delete();
+            outFile.delete();
+        }
+        catch( IOException e ) {
+            System.err.println( "Cannot run renderer: " + e.getLocalizedMessage() );
+            return null;
+        }
+        return img;
     }
 }
