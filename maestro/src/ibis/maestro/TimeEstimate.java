4a9d87475b3c6700734cb4c265370783733e5b87
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-06-16 09:15:43 +0000

    + Revert to a much simpler time estimate.

--- maestro/src/ibis/maestro/TimeEstimate.java
+++ maestro/src/ibis/maestro/TimeEstimate.java
@@ -7,19 +7,11 @@ package ibis.maestro;
  */
 class TimeEstimate
 {
-    private static final int BUFFER_SIZE = 40;
-    private int sampleCount = 0;
-    private long samples[] = new long[BUFFER_SIZE];
-    private long sampleTimes[] = new long[BUFFER_SIZE];
-    private int nextSampleIndex = 0;
-    private long decayInterval = Long.MAX_VALUE;
-    private int firstSample = 0;
+    private long average;
 
     TimeEstimate( long initial )
     {
-        samples[0] = initial;
-        sampleTimes[0] = 0;   // A long time ago.
-        nextSampleIndex++;
+        average = initial;
     }
 
     /**
@@ -28,7 +20,7 @@ class TimeEstimate
     @Override
     public String toString()
     {
-        return "average=" + Service.formatNanoseconds( getAverage() ) + " (based on " + sampleCount + " samples)";
+        return "average=" + Service.formatNanoseconds( average );
     }
 
     /**
@@ -37,31 +29,6 @@ class TimeEstimate
      */
     long getAverage()
     {
-        long now = System.nanoTime();
-        double sum = 0L;
-        double weight = 1;
-        int ix = nextSampleIndex;
-        double totalWeight = 0;
-        long interval = decayInterval;
-
-        do {
-            ix--;
-            if( ix<0 ) {
-                ix = BUFFER_SIZE-1;
-            }
-            while( (now-sampleTimes[ix])>interval ) {
-                // We crossed a weight treshold, decrease the
-                // weight of this and earlier samples.
-                interval += interval;
-                weight *= 0.5;
-            }
-            sum += samples[ix]/weight;
-            totalWeight += 1/weight;
-        } while( ix != firstSample );
-        long average = Math.round( sum/totalWeight );
-        if( sampleCount>4 ) {
-            decayInterval = 5*average;
-        }
         return average;
     }
 
@@ -71,20 +38,6 @@ class TimeEstimate
      */
     void addSample( long val )
     {
-        long now = System.nanoTime();
-        
-        samples[nextSampleIndex] = val;
-        sampleTimes[nextSampleIndex] = now;
-        if( firstSample == nextSampleIndex ) {
-            firstSample++;
-            if( firstSample>=BUFFER_SIZE ) {
-                firstSample = 0;
-            }
-        }
-        nextSampleIndex++;
-        if( nextSampleIndex>=BUFFER_SIZE ) {
-            nextSampleIndex = 0;
-        }
-        sampleCount++;
+        average = (2*average+val)/3;
     }
 }
