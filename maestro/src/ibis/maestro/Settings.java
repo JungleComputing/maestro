44b8d0cf441bc5851b0d6b5a844d03c02b342e10
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-06-18 14:03:00 +0000

    + Removed some debugging flags.

--- maestro/src/ibis/maestro/Settings.java
+++ maestro/src/ibis/maestro/Settings.java
@@ -21,31 +21,31 @@ class Settings {
     // Debugging flags.
     
     /** Trace type handling? */
-    static final boolean traceTypeHandling = true;
+    static final boolean traceTypeHandling = false;
 
     /** Trace the progress of workers? */
-    static final boolean traceWorkerProgress = true;
+    static final boolean traceWorkerProgress = false;
 
     /** Trace the handling or result jobs? */
     static final boolean traceResultJobs = false;
 
     /** Trace the progress of the worker lists of masters. */
-    static final boolean traceWorkerList = true;
+    static final boolean traceWorkerList = false;
     
     /** Trace the creation and destruction of Nodes. */
     static final boolean traceNodes = false;
 
     /** Trace the events in the master queue? */
-    static final boolean traceMasterQueue = true;
+    static final boolean traceMasterQueue = false;
 
     /** Trace the administration of remaining task time. */
     static final boolean traceRemainingTaskTime = false;
 
     /** Trace the progress of masters? */
-    static final boolean traceMasterProgress = true;
+    static final boolean traceMasterProgress = false;
 
     /** Trace all send events. */
-    static final boolean traceSends = true;
+    static final boolean traceSends = false;
 
     /** We limit the master queue to this many jobs per known worker. */
     static final int JOBS_PER_WORKER = 10;
