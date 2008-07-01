d165c7364061604ddaae160e729f0d896dbc0144
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-07-01 08:49:24 +0000

    + Disabled some tracing.

--- maestro/src/ibis/maestro/Settings.java
+++ maestro/src/ibis/maestro/Settings.java
@@ -24,7 +24,7 @@ class Settings {
     static final boolean traceTypeHandling = false;
 
     /** Trace the progress of workers? */
-    static final boolean traceWorkerProgress = true;
+    static final boolean traceWorkerProgress = false;
 
     /** Trace the handling or result tasks? */
     static final boolean traceResultTasks = false;
@@ -36,7 +36,7 @@ class Settings {
     static final boolean traceNodes = false;
 
     /** Trace the events in the master queue? */
-    static final boolean traceMasterQueue = true;
+    static final boolean traceMasterQueue = false;
 
     /** Trace the administration of remaining job time. */
     static final boolean traceRemainingJobTime = false;
