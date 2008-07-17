1b2877b8359a1ed7b45d60d45dda327f8a9e29e8
Kees van Reeuwijk <keesvr@xs4all.nl>
2008-07-17 11:00:46 +0000

    + Open up some of the label tracker methods.

---
+++ maestro/src/ibis/maestro/LabelTracker.java
@@ -0,0 +1,103 @@
+package ibis.maestro;
+
+import java.io.Serializable;
+import java.util.TreeSet;
+
+/**
+ * Hands out labels, and keeps track of the ones that have been returned.
+ * Allow labels to be returned out of order and more than once. At all
+ * times we can tell whether there are still outstanding labels, or
+ * that all labels we have handed out have been returned.
+ * 
+ * The implementation assumes that the set of returned values is mainly,
+ * but not entirely, in order. The bulk of the set is therefore represented
+ * by a range starting from 0. Any out-of-order labels are stored
+ * separately in a hashmap, that we try to transfer to the range
+ * at all new arrivals of labels.
+ * 
+ * @author Kees van Reeuwijk
+ *
+ */
+public class LabelTracker {
+    private static long labelValue = 0L;
+    private static final boolean trace = false;
+
+    /** The first label not in the bulk range. */
+    private long endOfRange = 0L;
+
+    private static final TreeSet<Long> set = new TreeSet<Long>();
+
+    /**
+     * A label as handed out by the tracker. It is entirely opaque.
+     */
+    public static class Label implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private final long value;
+
+	Label( long value )
+	{
+	    this.value = value;
+	}
+	
+	/**
+	 * Returns a string representation of this label.
+	 * @return The string representation of this label.
+	 */
+	@Override
+	public String toString()
+	{
+	    return "label#" + value;
+	}
+    }
+    
+    /**
+     * Get the next label from the tracker.
+     * @return The label.
+     */
+    public synchronized Label nextLabel()
+    {
+	Label res = new Label( labelValue++ );
+        if( trace ){
+            System.out.println( "nextLabel(): handed out " + res );
+        }
+        return res;
+    }
+    
+    /**
+     * Returns the given label to our administration.
+     * We're not shocked if the same label is returned more than once,
+     * or out of order.
+     * @param l The label we return.
+     */
+    @SuppressWarnings("synthetic-access")
+    public synchronized void returnLabel( Label l )
+    {
+        if( trace ){
+            System.out.println( "returnLabel(): got back " + l );
+        }
+	long val = l.value;
+	if( val<endOfRange ) {
+	    // Already covered by the range. Nothing to do.
+	    return;
+	}
+	set.add( val );
+	
+	// Try to enlarge the range with elements in the set.
+	while( set.contains( endOfRange ) ) {
+	    set.remove( endOfRange );
+	    endOfRange++;
+	}
+        if( trace ){
+            System.out.println( "returnLabel(): endOfRange=" + endOfRange + "; set size: " + set.size() );
+        }
+    }
+
+    /**
+     *  Returns true iff all labels we handed out have been returned.
+     * @return True iff all handed out labels have been returned.
+     */
+    synchronized public boolean allAreReturned()
+    {
+	return endOfRange == labelValue;
+    }
+}
