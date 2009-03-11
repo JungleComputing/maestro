53971023adfabd910ff355730d2715c4e379906b
Kees van Reeuwijk <keesvr@xs4all.nl>
2009-03-11 22:24:57 +0000

    Resolved conflict.

--- apps/VideoPlayer/src/ibis/videoplayer/BuildFragmentJob.java
+++ apps/VideoPlayer/src/ibis/videoplayer/BuildFragmentJob.java
@@ -4,94 +4,94 @@
 package ibis.videoplayer;
 
 import ibis.maestro.Job;
-import ibis.maestro.SeriesJob;
 import ibis.maestro.ParallelJob;
 import ibis.maestro.ParallelJobHandler;
+import ibis.maestro.SeriesJob;
 
 /**
  * @author Kees van Reeuwijk
  * 
  */
 final class BuildFragmentJob implements ParallelJob {
-    private static final long serialVersionUID = 6769001575637882594L;
-    private Job fetchJob;
-    int startFrame;
-    int endFrame;
-    RGB48Image frames[];
+	private static final long serialVersionUID = 6769001575637882594L;
+	private final Job fetchJob;
+	int startFrame;
+	int endFrame;
+	RGB48Image frames[];
 
-    BuildFragmentJob(Job fetchJob) {
-        this.fetchJob = fetchJob;
-    }
+	BuildFragmentJob(Job fetchJob) {
+		this.fetchJob = fetchJob;
+	}
 
-    static SeriesJob createGetFrameJob() {
-        return new SeriesJob( new FetchFrameJob(),
-                new DecompressFrameJob(), new ColourCorrectJob(),
-                new ScaleFrameJob(2));
-    }
+	static SeriesJob createGetFrameJob() {
+		return new SeriesJob( new FetchFrameJob(),
+				new DecompressFrameJob(), new ColourCorrectJob(),
+				new ScaleFrameJob(2));
+	}
 
-    /**
-     * @return True, because this job can run anywhere.
-     */
-    @Override
-    public boolean isSupported() {
-        return true;
-    }
+	/**
+	 * @return True, because this job can run anywhere.
+	 */
+	@Override
+	public boolean isSupported() {
+		return true;
+	}
 
-    /**
-     * Returns the result of the reduction.
-     * @return The result.
-     */
-    public Object getResult() {
-        int sz = 0;
+	/**
+	 * Returns the result of the reduction.
+	 * @return The result.
+	 */
+	public Object getResult() {
+		int sz = 0;
 
-        for (int i = 0; i < frames.length; i++) {
-            RGB48Image frame = frames[i];
-            if (frame != null) {
-                sz += frame.data.length;
-            }
-        }
-        short data[] = new short[sz];
-        int ix = 0;
-        for (int i = 0; i < frames.length; i++) {
-            RGB48Image frame = frames[i];
-            if (frame != null) {
-                System.arraycopy(frame.data, 0, data, ix, frame.data.length);
-                ix += frame.data.length;
-            }
-        }
-        VideoFragment value = new VideoFragment(startFrame, endFrame, data);
-        if (Settings.traceFragmentBuilder) {
-            System.out.println("Sending fragment [" + startFrame + "..."
-                    + endFrame + "]");
-        }
-        return value;
-    }
+		for (int i = 0; i < frames.length; i++) {
+			final RGB48Image frame = frames[i];
+			if (frame != null) {
+				sz += frame.data.length;
+			}
+		}
+		final short data[] = new short[sz];
+		int ix = 0;
+		for (int i = 0; i < frames.length; i++) {
+			final RGB48Image frame = frames[i];
+			if (frame != null) {
+				System.arraycopy(frame.data, 0, data, ix, frame.data.length);
+				ix += frame.data.length;
+			}
+		}
+		final VideoFragment value = new VideoFragment(startFrame, endFrame, data);
+		if (Settings.traceFragmentBuilder) {
+			System.out.println("Sending fragment [" + startFrame + "..."
+			                                      + endFrame + "]");
+		}
+		return value;
+	}
 
-    /**
-     * @param input The input for the computation.
-     * @param handler The map/reduce handler assigned to this computation.
-     */
-    public void split(Object input, ParallelJobHandler handler) {
-        FrameNumberRange range = (FrameNumberRange) input;
-        if (Settings.traceFragmentBuilder) {
-            System.out.println("Collecting frames for fragment " + range);
-        }
-        startFrame = range.startFrameNumber;
-        endFrame = range.endFrameNumber;
-        frames = new RGB48Image[1+endFrame-startFrame];
-        for (int frame = startFrame; frame <= endFrame; frame++) {
-            Integer frameno = new Integer(frame);
-            handler.submit(frameno, frameno, fetchJob);
-        }
-    }
+	/**
+	 * @param input The input for the computation.
+	 * @param handler The map/reduce handler assigned to this computation.
+	 */
+	public void split(Object input, ParallelJobHandler handler) {
+		final FrameNumberRange range = (FrameNumberRange) input;
+		if (Settings.traceFragmentBuilder) {
+			System.out.println("Collecting frames for fragment " + range);
+		}
+		startFrame = range.startFrameNumber;
+		endFrame = range.endFrameNumber;
+		frames = new RGB48Image[1+endFrame-startFrame];
+		for (int frame = startFrame; frame <= endFrame; frame++) {
+			final Integer frameno = new Integer(frame);
+			handler.submit(frameno, frameno, fetchJob);
+		}
+	}
 
-    /**
-     * Handle results as they arrive.
-     * @param id The id of the result.
-     * @param result The result.
-     */
-    public void merge(Object id, Object result) {
-        int ix = (Integer) id;
-        frames[ix] = (RGB48Image) result;
-    }
+	/**
+	 * Handle results as they arrive.
+	 * @param id The id of the result.
+	 * @param result The result.
+	 */
+	public void merge(Object id, Object result) {
+		final int ix = (Integer) id;
+		frames[ix] = (RGB48Image) result;
+	}
 }
